0a8fbded6c57cc5ce8757804d1678b228dc9b661
lagofelipe <lagofelipe@gmail.com>
2020-11-16 18:35:53 -0600

    done

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+let win = array.find(array => array.result === "W")
+return win ? win.year : undefined
+
+}
